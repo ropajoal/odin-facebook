8a7baf2c47c33ccf7cb56e0a35742674b0cac457
ropajoal <ropajoal@hotmail.es>
2022-05-19 20:22:22 -0400

    Rename mailer to User

--- test/mailers/previews/welcome_email_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/welcome_email_mailer
-class WelcomeEmailMailerPreview < ActionMailer::Preview
-
-end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class WelcomeEmailMailerTest < ActionMailer::TestCase
+class UserMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
